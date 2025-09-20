264f602d3c03e804422b3e8707751a3977f05675
47pakistanimian-png <47pakistanimian@gmail.com>
2025-09-20 23:37:02 +0500

    Update signature.js

--- api/signature.js
+++ api/signature.js
@@ -3,7 +3,12 @@ const crypto = require('crypto');
 export default function handler(req, res) {
   const { transformation, timestamp } = req.query;
   
-  const params = { timestamp, transformation };
+  const params = { 
+    eager_async: 'true',
+    timestamp, 
+    transformation 
+  };
+  
   const sortedParams = Object.keys(params)
     .sort()
     .map(key => `${key}=${params[key]}`)

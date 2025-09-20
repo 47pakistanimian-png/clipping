513cfc547f887063cbc74af0e663f6573d16b446
47pakistanimian-png <47pakistanimian@gmail.com>
2025-09-20 23:12:14 +0500

    Create signature.js

---
+++ api/signature.js
@@ -0,0 +1,18 @@
+const crypto = require('crypto');
+
+export default function handler(req, res) {
+  const { transformation, timestamp } = req.query;
+  
+  const params = { timestamp, transformation };
+  const sortedParams = Object.keys(params)
+    .sort()
+    .map(key => `${key}=${params[key]}`)
+    .join('&');
+    
+  const signature = crypto
+    .createHash('sha1')
+    .update(sortedParams + process.env.CLOUDINARY_SECRET)
+    .digest('hex');
+    
+  res.json({ signature, timestamp });
+}
